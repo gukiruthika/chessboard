93d812598766956da4a2b0d695ad5cdb2c9e5e82
gukiruthika <112635198+gukiruthika@users.noreply.github.com>
2022-09-07 18:14:38 +0530

    Add files via upload

---
+++ Double_value_Shift.java
@@ -0,0 +1,16 @@
+package javaProject1.pack1;
+
+import java.util.Scanner;
+
+public class Double_value_Shift {
+
+	public static void main(String[] args) {
+		Scanner sn = new Scanner(System.in);
+		int base=sn.nextInt();
+		int noOfTerms=sn.nextInt();
+		int result = base << (noOfTerms-1);
+		System.out.println(result);   
+
+	}
+
+}
